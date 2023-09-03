f1898a2f25138bb885fed38f0b3377c7f1d77e4b
Aishwarya0206 <aishustarz02@gmail.com>
2023-09-04 00:38:51 +0530

    Input/Output completed

---
+++ Input_Output/1.py
@@ -0,0 +1,21 @@
+'''
+
+Write a code to get the input in the given format and print the output in the given format
+
+Input Description:
+To take an integer value
+
+Output Description:
+Print the integer value
+
+Sample Input :
+2
+
+Sample Output :
+2
+
+'''
+
+#Getting input via STDIN
+userInput = int(input())
+print("{0}".format(userInput))
---
+++ Input_Output/2.py
@@ -0,0 +1,23 @@
+'''
+
+Write a code to get the input in the given format and print the output in the given format
+
+Input Description:
+A single line contains integers separated by space
+
+Output Description:
+Print the integer list of integers separated by space
+
+Sample Input :
+2 3 4 5 6 7 8
+
+Sample Output :
+2 3 4 5 6 7 8
+
+'''
+
+
+
+#Getting input via STDIN
+userInput = [x for x in input().split()]
+print(' '.join(userInput))
